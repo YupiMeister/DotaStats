d869321534291918a3e88937795f5a0def109574
YupiMeister <slavapopov399@gmail.com>
2020-04-03 19:11:55 +0300

    Logcat

--- app/src/main/java/bmstu/badplayers/dotastats/MainActivity.kt
+++ app/src/main/java/bmstu/badplayers/dotastats/MainActivity.kt
@@ -2,11 +2,20 @@ package bmstu.badplayers.dotastats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import bmstu.badplayers.dotastats.steamweb.SteamOpenID
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val steamOpenID = SteamOpenID()
+        }
     }
 }
